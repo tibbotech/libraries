e0c97407a89f2dfd7e56a8d307dd9575d2b67ede
Jimmy Hu <jimmy.hu@tibbo.com>
2023-01-05 09:28:31 +0800

    update tests

--- .github/test.sh
+++ .github/test.sh
@@ -1,3 +1,3 @@
-docker run -v ${PWD}/:/TIDEProjects/ tibbotech/tidewine /bin/sh -c \
+docker run -v ${PWD}/:/TIDEProjects/ --rm tibbotech/tidewine /bin/sh -c \
 "wine /home/tibbo/.wine/drive_c/Program\ Files/Tibbo/TIDE/Bin/tmake.exe \"C:\\users\\tibbo\\TIDEProjects\\.tests\\testcompile\\tblsortts.tpr\" \
 -p \"C:\\users\\tibbo\\TIDEProjects\\.tests\\Platforms\" -r"
